e01830945f9fb384bb8625f6938aab5126a87883
Programadora-Roelita <rtellog@isagustin.edu.pe>
2023-09-08 19:17:14 -0500

    Coloque codigo al boton sumar y ademas agregue los botones restar, multiplicar y dividit

--- src/Calculadora_IU.java
+++ src/Calculadora_IU.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -31,6 +34,9 @@ public class Calculadora_IU extends javax.swing.JFrame {
         btnSumar = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        txtRestar = new javax.swing.JButton();
+        txtMultiplicar = new javax.swing.JButton();
+        txtDividir = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -41,10 +47,26 @@ public class Calculadora_IU extends javax.swing.JFrame {
         });
 
         btnSumar.setText("SUMAR");
+        btnSumar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSumarActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("NRO2");
 
         jLabel2.setText("NRO1");
+
+        txtRestar.setText("RESTAR");
+
+        txtMultiplicar.setText("MULTIPLICAR");
+
+        txtDividir.setText("DIVIDIR");
+        txtDividir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtDividirActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -57,7 +79,14 @@ public class Calculadora_IU extends javax.swing.JFrame {
                     .addComponent(jLabel1))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnSumar)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnSumar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtRestar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtMultiplicar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtDividir))
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                         .addComponent(txtnro2, javax.swing.GroupLayout.DEFAULT_SIZE, 361, Short.MAX_VALUE)
                         .addComponent(txtnro1)))
@@ -75,7 +104,11 @@ public class Calculadora_IU extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(txtnro2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(btnSumar)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSumar)
+                    .addComponent(txtRestar)
+                    .addComponent(txtMultiplicar)
+                    .addComponent(txtDividir))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -85,6 +118,21 @@ public class Calculadora_IU extends javax.swing.JFrame {
     private void txtnro2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtnro2ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txtnro2ActionPerformed
+
+    private void btnSumarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSumarActionPerformed
+        // TODO add your handling code here:
+
+        double nro1 = Double.parseDouble(txtnro1.getText());
+        double nro2 = Double.parseDouble(txtnro2.getText());
+        double sumar= nro1+nro2;
+        JOptionPane.showMessageDialog(this, "La suma es:" +sumar);
+
+
+    }//GEN-LAST:event_btnSumarActionPerformed
+
+    private void txtDividirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtDividirActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtDividirActionPerformed
 
     /**
      * @param args the command line arguments
@@ -125,6 +173,9 @@ public class Calculadora_IU extends javax.swing.JFrame {
     private javax.swing.JButton btnSumar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton txtDividir;
+    private javax.swing.JButton txtMultiplicar;
+    private javax.swing.JButton txtRestar;
     private javax.swing.JTextField txtnro1;
     private javax.swing.JTextField txtnro2;
     // End of variables declaration//GEN-END:variables
